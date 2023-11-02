50e95fb1a4e0716ec6ee3ff8901e9be28107f2b7
hydra.h8 <muthanandham.g@hash8.io>
2023-11-02 10:29:07 +0530

    First command

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FirstComponent,
+  },
+];
 
 @NgModule({
+  // declarations: [AppComponent, FirstComponent],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
